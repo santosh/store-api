75e87f158eb94b758f44373be4896fb94e21825f
Santosh Kumar <sntshkmr60@gmail.com>
2019-09-02 14:55:28 +0530

    PUT method with database interaction

--- app/item.py
+++ app/item.py
@@ -75,14 +75,33 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if not item:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
 
+        if not item:
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}, 500
+        else:
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the item"}, 500
+
+        return updated_item
+    
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('db.sqlite')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
+        
 
 class ItemList(Resource):
     def get(self):
