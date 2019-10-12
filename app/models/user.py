d0dbc18e6677c8e856632457c1a13acb4644bdab
Santosh Kumar <sntshkmr60@gmail.com>
2019-10-13 03:01:55 +0530

    Adds email field to sent confirmation email to

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, default=False)
 
     @classmethod
